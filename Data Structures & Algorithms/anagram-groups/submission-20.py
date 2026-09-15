39c94a7040a0669bf62ee1ee4d9f38c7860f94b6
Edward Bordenave <edward.bordenave@gmail.com>
2026-09-15 05:53:09 -0400

    Add: anagram-groups - submission-20

---
+++ Data Structures & Algorithms/anagram-groups/submission-20.py
@@ -0,0 +1,16 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        groups = {}
+
+        for word in strs:
+            sorted_word = ''.join(sorted(word))
+
+            if sorted_word not in groups:
+                groups[sorted_word] = [word]
+
+            else:
+                groups[sorted_word].append(word)
+
+        return [val for val in groups.values()]
+        
